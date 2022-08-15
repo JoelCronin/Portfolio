b33f0d4eeddf2bc9aded70708a26c74dcd692fcb
joel cronin <you@example.com>
2022-08-15 16:13:19 -0400

    add

---
+++ README.md
@@ -0,0 +1,50 @@
+# React Portfolio
+
+![License](https://img.shields.io/badge/license-Unlicense-green)
+
+In this project, I used my knowledge of React to build a portfolio that I can present to employers for my upcoming job search. It has four sections:
+
+* About Me
+* Completed Projects 
+* Contact
+* Resume
+
+I used the component, props and states functionality provided by React to add some dynamism to the portfolio and enjoyed the process of making this site.
+
+![Screenshot](./src/assets/Screenshot.png)
+
+
+## Table of Contents
+
+[Installation](#installation)
+
+[Usage](#usage)
+
+[Contributing](#contributing)
+
+[License](#license)
+
+[Questions](#questions)
+
+[Links](#links)
+
+## Installation
+
+To install this you will need to run: 'npm i' 
+
+## Usage
+
+To start this locally you will need to run npm start.
+
+## Contributing
+
+To contribute to this, clone the repo locally and commit on a seperate branch
+
+## License
+
+This application is covered under the following license: The Unlicence
+
+## Links
+
+You can see the deployed version of this by [clicking here](https://joelcronin.github.io/cautious-dollop/)
+
---
+++ src/components/Resume.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+
+//Resume Component
+function Resume() {
+  return (
+    <div className='skills'>
+      <div id='skillTitle'> <br />
+      <h4> Skills and Proficiencies:</h4> <br/>
+      </div>
+      <div id='skillPoints'>
+        <div className='frontend'>
+          <h4 className='endType'>Front End</h4> <br />
+          <ul>
+            <li>HTML5</li>
+            <li>CSS</li>
+            <li>JavaScript / ES6+</li>
+            <li>JQuery</li>
+            <li>Responsive Design</li>
+            <li>React.js</li>
+            <li>Bootstrap</li>
+            <li>Bulma</li>
+          </ul> <br />
+        </div>
+        <div id='backend'>
+        <h4 className='endType'>Back End</h4><br />
+          <ul>
+            <li>APIs</li>
+            <li>Node.js</li>
+            <li>Express.js</li>
+            <li>SQL / MySQL / Sequelize</li>
+            <li>Inquirer</li>
+            <li>MongoDB / Mongoose</li>
+            <li>GraphQL</li>
+            <li>JSON</li>
+          </ul> <br /> 
+        </div>
+      </div>
+      <div className='general'>
+      
+        <ul className='last' >
+        <h4 className='resumeHead'>General</h4> <br></br>
+            <li>MERN Stack</li>
+            <li>MVC Framework</li>
+            <li>GitHub</li>
+            <li>Heroku</li>
+            
+        </ul> <br></br><br />
+        
+      </div>
+      <br></br> <br></br><h4 className='resumeHead'>A copy of my Resume can be downloaded <a href='https://drive.google.com/file/d/1D6-nPLz84zxxUJtwQucDHUmnEFuTJvTV/view?usp=sharing' target= 'blank'>here</a></h4><br />
+    </div>
+  )
+}
+
+export default Resume
